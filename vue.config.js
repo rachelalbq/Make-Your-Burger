4e9d3792a7c1acafdc334dbb15ba9c855c6a16b6
rachelalbq <rachel.alb.97@hotmail.com>
2024-06-11 17:39:18 -0300

    env

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Make-Your-Buger/'
+    ? '/Make-Your-Burger/'
     : '/'
 })
