4f29c1c789faa86437fffb949e09b7a49c70312b
rachelalbq <rachel.alb.97@hotmail.com>
2024-06-11 12:37:09 -0300

    feat: initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <Banner/>
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <p>forms</p>
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+
+export default {
+  components: {
+    Banner
+  }
+}
+</script>
---
+++ src/views/Orders.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main-container">
+    <h1>Pedidos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Orders'
+}
+</script>
